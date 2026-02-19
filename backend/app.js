62a23f053f00ed1e3e9c901ddfd1d261f96e3b0b
RanasingheDD <dishand398@gmail.com>
2026-02-19 16:49:24 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/health', (req, res) => {
-  res.json({ message: "Hello from Backend!" });
+  res.json({ message: "Hi from Backend!" });
 });
 
 app.listen(PORT, () => {
